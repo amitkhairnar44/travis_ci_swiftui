ef247518bca729850cc4fdcb8a46424d7c8f83f4
Amit Khairnar <amitskhairnar@icloud.com>
2021-02-17 20:26:52 +0530

    Initial Commit

---
+++ Travis CI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Travis CI
+//
+//  Created by Amit Khairnar on 17/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Travis CI/Travis_CIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travis_CIApp.swift
+//  Travis CI
+//
+//  Created by Amit Khairnar on 17/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Travis_CIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
